d5019c405ca2efbec7654e6e6d08f23b90464475
RobertoAssis <robertoasgon@gmail.com>
2025-01-19 18:18:37 -0400

    update: map

--- otserver/datapack/data/spells/scripts/jutsus/healing/regeneration.lua
+++ otserver/datapack/data/spells/scripts/jutsus/healing/regeneration.lua
@@ -1,7 +1,6 @@
 local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_HEALING)
-combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
-combat:setParameter(COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
+combat:setParameter(COMBAT_PARAM_EFFECT, 2)
 combat:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
 
 function onGetFormulaValues(player, level, magicLevel)
